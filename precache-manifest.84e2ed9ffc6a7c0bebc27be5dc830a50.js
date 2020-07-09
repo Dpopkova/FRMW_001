c80e5b7d2d3bf0aca28460dcff5034b4b1efebe5
Dpopkova <daria.ol.p@gmail.com>
2020-07-09 22:00:15 +0300

    Updates

---
+++ precache-manifest.84e2ed9ffc6a7c0bebc27be5dc830a50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7793e3e79082c915b2aeffb9616193",
+    "url": "/FRMW_001/index.html"
+  },
+  {
+    "revision": "41f156a7da99e1a4ed7d",
+    "url": "/FRMW_001/static/css/main.0e11fa8b.chunk.css"
+  },
+  {
+    "revision": "4bbb4f0f76db72d34e76",
+    "url": "/FRMW_001/static/js/2.b554ae94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/FRMW_001/static/js/2.b554ae94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f156a7da99e1a4ed7d",
+    "url": "/FRMW_001/static/js/main.7e36b4c7.chunk.js"
+  },
+  {
+    "revision": "05052cab6a4e552f1744",
+    "url": "/FRMW_001/static/js/runtime-main.3860b459.js"
+  }
+]);
